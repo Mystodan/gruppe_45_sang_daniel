0f6cdf30b3023cd9cb3ce17b6d3c9025c46499e6
sangnn <sangnn@stud.ntnu.no>
2021-04-14 19:50:08 +0200

    Added functions

--- source/source/funksjoner.h
+++ source/source/funksjoner.h
@@ -2,9 +2,10 @@
 #ifndef __FUNKSJONER_H
 #define __FUNKSJONER_H
 
-void skrivMeny() {
-
-
-}
+void skrivMeny();
+void lesFraFil();
+void skrivTilFil();
+void nyKunde();
+void nyUtleiested();
 
 #endif
